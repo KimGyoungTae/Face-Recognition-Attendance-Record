d1f43fc0247ddcafa9303e0a5354c799597d28e9
KimGyoungTae <83820089+KimGyoungTae@users.noreply.github.com>
2023-01-14 22:48:02 +0900

    Add files via upload
    IndexError: list index out of range (IndexError: 목록 인덱스가 범위를 벗어남) 오류 발생

--- FaceAttendance.py
+++ FaceAttendance.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
-from datetime import datetime
+from datetime import datetime # 시간과 날짜에 대한 라이브러리
 
 #이미지 가져올 목록 생성
 path = 'faces'
@@ -25,19 +25,19 @@ def findEncodings(images):
         encode = face_recognition.face_encodings(img)[0]  # 감지할 얼굴 인코딩, 첫번째 요소만 가져오기
         encodeList.append(encode) #인코딩 찾은것을 목록에 추가
     return encodeList #목록들 반환
-''''
+
 def markAttendance(name):
-    with open('Attendance.csv','r+') as f:
+    with open('Attendance.csv','r+') as f:  # 동시에 읽고 쓰기를 원함.
         myDataList = f.readlines()
         nameList = []
         for line in myDataList:
-            entry = line.split(',')
-            nameList.append(entry[0])
-        if name not in nameList:
+            entry = line.split(',') # ',' 기준으로 분할
+            nameList.append(entry[0]) # 첫 번째 요소를 추가, entry[0]은 이름이 될 것이고, 목록의 이름만 추가하게 된다.
+        if name not in nameList:    #이름 목록에 이름이 있는지 확인하기 위함.
             now = datetime.now()
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
-'''''
+
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
@@ -47,27 +47,28 @@ cap = cv2.VideoCapture("http://192.168.35.193:8080/video") #IP webcam의 주소
 
 while True:     #실시간으로 비디오 재생을 위해 while 반복문 사용
     success, img = cap.read() # 비디오가 있는지 없는지 bool 체크, 비디오 자체를 나타내는 img 변수
-    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)  # 픽셀크기는 정의하지 않고, 스케일을 0.25로 지정함.
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)  # RGB로 변환
 
-    faceCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS,faceCurFrame) # 감지할 얼굴 인코딩, 첫번째 요소만 가져오기
+    faceCurFrame = face_recognition.face_locations(imgS) # 이미지에서의 인코딩 위치 찾기
+    encodesCurFrame = face_recognition.face_encodings(imgS,faceCurFrame) # 감지할 얼굴 인코딩, 프레임이 얼굴의 위치로 향하게
 
-    for encodeFace,faceLoc in zip(encodesCurFrame,faceCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace) #알려진 인코딩 목록과 인코딩 된 얼굴과 비교하기
+    for encodeFace,faceLoc in zip(encodesCurFrame,faceCurFrame): # 현재 프레임에서 인코딩된 얼굴과 프레임 상에서의 얼굴의 위치를 가져온다.
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace) #알려진 인코딩 목록과 현재 프레임에서 찾은 인코딩 된 얼굴과 비교하기
         faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
-        print(faceDis)
+        print(faceDis)  # 상대적으로 가장 낮은게 가장 일치하다고 볼 수 있다.
         matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
+            name = classNames[matchIndex].upper() # 만약 3개의 이미지 자료가 있고, 웹켐에 보여지는 얼굴과 비교한 상황 => 3개의 이미지 자료 중 가장 낮은 수치가 얼굴과 가장 정확하다고 볼 수 있다.
+            # upper()은 가장 정확한 이미지 이름을 소문자로 되어있다면 대문자로 변환하여 출력시켜준다.
             # print(name)
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)  # 웹켐 상에서 보여지는 얼굴에 네모난 직사각형이 그려짐.
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-       #     markAttendance(name)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)   # 직사각형으로 가리키고 있는 얼굴이 무엇인지 화면에 표시해줌.
+            markAttendance(name) # 얼굴을 찾을 때마다 함수 호출
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord("q"): #while 무한루프를 빠져나오기 위한 키보드 q를 눌러 WebCam 사용 중지
